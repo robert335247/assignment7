8ea69315ff294bb136843466bca33c4926626490
robert <robert.335247@gmail.com>
2025-05-14 00:08:27 +0800

    fix icon constructor1

--- include/AnsiPrint.h
+++ include/AnsiPrint.h
@@ -1,12 +1,14 @@
 #ifndef _ANSIPRINT_H //d
 #define _ANSIPRINT_H
 
+using namespace std;
+
 //#define _WIN
 enum color {black=0, red, green, yellow, blue, pink, cyan, white, nochange};
 
 // 使用指定的 ANSI 顏色選項來格式化並輸出字串
-void AnsiPrint(const char *str, color fg, color bg = nochange, 
+string AnsiPrint(const char *str, color fg, color bg = nochange, 
                    bool hi = false, bool blinking = false);
-void AnsiPrint(const char *str, bool hi = false, bool blinking = false);
+string AnsiPrint(const char *str, bool hi = false, bool blinking = false);
 
 #endif
